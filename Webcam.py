d21a183f11f5f5f42487428df87b10540093200b
Panithan <83627892+PanithanS@users.noreply.github.com>
2024-03-17 20:49:26 +0900

    Update Webcam.py
    correct some typo

--- Webcam.py
+++ Webcam.py
@@ -36,7 +36,7 @@ class Webcam:
                 self.start_x1, self.start_y1, self.start_x2, self.start_y2 = map(int,(file.readlines(0))[0].split(","))
             self.draw_count = self.draw_count + 1
         except:
-            #The start box (x, y), which we will draw latter
+            #The start box (x, y), which we will draw later
             self.start_x1, self.start_y1 = 0, 0
             self.start_x2, self.start_y2 = 0, 0
 
@@ -45,7 +45,7 @@ class Webcam:
                 self.end_x1, self.end_y1, self.end_x2, self.end_y2 = map(int,(file.readlines(0))[0].split(","))
             self.draw_count = self.draw_count + 1
         except:
-            #The end box (x, y), which we will draw latter
+            #The end box (x, y), which we will draw later
             self.end_x1, self.end_y1 = 0, 0
             self.end_x2, self.end_y2 = 0, 0
 
@@ -54,24 +54,24 @@ class Webcam:
         function to close the webcam window, and release the webcam
         """
         try:
-            cv2.destroyWindow(self.cam_name) #In some version of openCV, this give an error
+            cv2.destroyWindow(self.cam_name) #In some versions of openCV, this gives an error
         except:
-            self.key = 27 #Equivalent to 'ESC'
+            self.key = 27 #Equivalent to pushing the 'ESC' button on keyboard
         self.active = False
         self.cap.release()
     
     def is_quit(self):
         """
-        function to check if user try to close the webcam window
+        function to check if the user tries to close the webcam window
         """
         if self.key==27: # key =27 is the ESC key
             self.killcam() # kill this Webcam
-        elif cv2.getWindowProperty(self.cam_name, cv2.WND_PROP_VISIBLE) < 1: # cv2.WND_PROP_VISIBLE = 0 when user click on "X" on the camera window
+        elif cv2.getWindowProperty(self.cam_name, cv2.WND_PROP_VISIBLE) < 1: # cv2.WND_PROP_VISIBLE = 0 when the user clicks on "X" of the Webcam window
             self.killcam() # kill this Webcam
     
     def mouse_and_keys(self, event, x, y, flags, param):
         """
-        function to get mouse event in the webcam window
+        function to get a mouse event in the webcam window
         """
         
         # getmouse position
@@ -84,10 +84,10 @@ class Webcam:
             self.y1_draw = y 
             self.x1_draw = x
 
-            if self.draw_count%2 == 0: #True if we drawing the 'start point'
+            if self.draw_count%2 == 0: #True if we are drawing the 'start point'
                 self.start_y1 = self.y1_draw 
                 self.start_x1 = self.x1_draw
-            else: #Ortherwise, we drawing the 'end point'
+            else: #Ortherwise, we are drawing the 'end point'
                 self.end_y1 = self.y1_draw 
                 self.end_x1 = self.x1_draw
                 
@@ -95,19 +95,19 @@ class Webcam:
             self.y2_draw = y
             self.x2_draw = x
             
-            if self.draw_count%2 == 0: #True if we drawing the 'start point'
+            if self.draw_count%2 == 0: #True if we are drawing the 'start point'
                 self.start_y2 = self.y2_draw 
                 self.start_x2 = self.x2_draw
                 
-                #Then save these point as "start_box.txt"
+                #Then save these points as "start_box.txt"
                 with open("SpeedCap\start_box.txt", "w+") as file:
                     file.write( str(self.start_x1) + "," + str(self.start_y1) + "," + str(self.start_x2) + "," + str(self.start_y2))
 
-            else: #Ortherwise, we drawing the 'end point'
+            else: #Ortherwise, we are drawing the 'end point'
                 self.end_y2 = self.y2_draw 
                 self.end_x2 = self.x2_draw
 
-                #Then save these point as "end_box.txt"
+                #Then save these points as "end_box.txt"
                 with open("SpeedCap\end_box.txt", "w+") as file:
                     file.write( str(self.end_x1) + "," + str(self.end_y1) + "," + str(self.end_x2) + "," + str(self.end_y2))
 
@@ -120,7 +120,7 @@ class Webcam:
 
         Args:
         img: an image
-        waittime (ms): delay time waiting for any keys press, also work as a delay between images to be displayed
+        waittime (ms): delay time waiting for any keys press, also works as a delay between images to be displayed
         display_time: overlay clock on the top-left of the image in "%H:%M:%S" form
         
         Returns:
@@ -146,7 +146,7 @@ class Webcam:
         cv2.putText(img, "START: " + str(self.started_time), (5, 45), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (128, 128, 128), 1)
         cv2.putText(img, "END: " + str(self.ended_time), (5, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (128, 128, 128), 1)
 
-        # Need to draw the estimate bounding box for startting and ending point 
+        # Need to draw the estimate bounding box for starting and ending points 
         if self.drawing:
             #tempolary show the drawing box
             cv2.rectangle(img, (self.x1_draw, self.y1_draw),(self.mouse_x, self.mouse_y),(0, 0, 0), 1)
@@ -162,13 +162,13 @@ class Webcam:
                 cv2.rectangle(img, (self.end_x1, self.end_y1),(self.end_x2, self.end_y2),(255, 49, 49), 2)
                 cv2.putText(img, str("END"), (self.end_x1, self.end_y1), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 49, 49), 2)
 
-            else: # when we drawing
-                if self.draw_count%2 == 0: #True if we drawing the 'start point'
+            else: # when we are drawing
+                if self.draw_count%2 == 0: #True if we are drawing the 'start point'
                     # show the end box while we draw the start box 
                     cv2.rectangle(img, (self.end_x1, self.end_y1),(self.end_x2, self.end_y2),(255, 49, 49), 2)
                     cv2.putText(img, str("END"), (self.end_x1, self.end_y1), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 49, 49), 2)
 
-                else: #Ortherwise, we drawing the 'end point'
+                else: #Ortherwise, we are drawing the 'end point'
                     # show the start point while we draw the end box 
                     cv2.rectangle(img, (self.start_x1, self.start_y1),(self.start_x2, self.start_y2),(76, 187, 23), 2)
                     cv2.putText(img, str("START"), (self.start_x1, self.start_y1), cv2.FONT_HERSHEY_SIMPLEX, 1, (76, 187, 23), 2)
@@ -181,7 +181,7 @@ class Webcam:
         
         self.is_quit() #if 'ESC' or 'click X' on window
 
-        self.sbb = [self.start_x1,self.start_y1,self.start_x2,self.start_y2]
-        self.ebb = [self.end_x1,self.end_y1,self.end_x2,self.end_y2]
+        self.sbb = [self.start_x1,self.start_y1,self.start_x2,self.start_y2] #starting bounding box
+        self.ebb = [self.end_x1,self.end_y1,self.end_x2,self.end_y2] #ending bounding box
 
         return self.sbb, self.ebb
